2661e5d05cd693d7e0b6c2244bf55ac3d52a5eae
Dario Piotrowicz <dario@cloudflare.com>
2025-04-08 15:58:34 +0100

    add readdir test

---
+++ tests/utils/index.ts
@@ -0,0 +1,33 @@
+import process from "node:process";
+import assert from "node:assert";
+import { spawn } from "node:child_process";
+
+type Example = {
+	url: string;
+	kill: () => void;
+};
+
+export async function runExample(exampleName: string): Promise<Example> {
+	return await new Promise((resolve) => {
+		const child = spawn("npm", ["run", "dev"], {
+			cwd: `./examples/${exampleName}`,
+		});
+
+		child.stdout.on("data", (data) => {
+			const dataStr = `${data}`;
+			const match = dataStr.match(
+				new RegExp(`\\[wrangler:inf\\] Ready on (?<url>http://localhost:\\d+)`)
+			);
+			if (match && match.groups?.url) {
+				const url = match.groups.url;
+				resolve({
+					url,
+					kill: () => {
+						assert(child.pid);
+						process.kill(child.pid);
+					},
+				});
+			}
+		});
+	});
+}
