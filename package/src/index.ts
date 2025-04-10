f6b34eaedcf6960196d4d82690ad7fcecb353b60
Dario Piotrowicz <dario@cloudflare.com>
2025-04-10 10:03:19 +0100

    remove old comment

--- package/src/index.ts
+++ package/src/index.ts
@@ -6,8 +6,6 @@ import { savePolyfills } from "./polyfills";
 import { generateManifestsIndex } from "./manifests";
 import { version } from "../package.json";
 
-// for nodejs.org assets should be './pages' and './snippets'
-
 async function main() {
 	console.log(
 		`\x1b[35m🛠️  Setting up Wrangler fs polyfills (v${version})\n\n\x1b[0m`
